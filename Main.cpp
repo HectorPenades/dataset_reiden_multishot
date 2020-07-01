9bfaee7fb0e0930048e410ad9f282ff48120c2fa
HectorPenades <67463445+HectorPenades@users.noreply.github.com>
2020-07-01 20:11:38 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	get_multiple_samples(prid, 7,4, number_of_b_samples,stride, random_b_samples);
 	for(int i=1; i<number_of_b_samples+1; i++)
         	train_val_test_division_multiple(prid, 100, 100, 100, 10, 100, 649, 100, &tag, i);
-    	create_pair_data_multiple(prid, 100000, 10000, 1, 1, 1);   //(prid, 100000, 10000, 1,4, 1);
+    	create_pair_data_multiple(prid, 100000, 10000, 1, 1, 1);   
     	for(int i=2; i<number_of_b_samples+1; i++)
         	create_pair_data_multiple_remaining(prid, i);
     	tag=true;
